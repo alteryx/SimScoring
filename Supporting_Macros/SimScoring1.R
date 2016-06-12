75538dd492d32fa30e22936e8c9894e63a7c08f0
Dylan Blanchard <dylblanchard@gmail.com>
2016-06-11 21:12:43 -0500

    update SimScoring1.R to match macro

--- Supporting_Macros/SimScoring1.R
+++ Supporting_Macros/SimScoring1.R
@@ -9,15 +9,32 @@ config <- list(
   seed = numericInput('%Question.seed%' , 50)
 )
 
-chunkSize <- config$numRecords
-isGLM <- config$isGLM
-mult <- config$per.iter
-seed <- config$seed
 
-## Inputs ----
-
-model <- unserializeObject(as.character(read.Alteryx("model")$Object[1]))
-totalCount <- as.integer(unlist(read.Alteryx("totalCountSim", mode="list"))[1])
-validation <- if (!isGLM) read.Alteryx("validation") else NULL
-
-scoreProcess(model, isGLM, mult, totalCount, validation, seed, chunkSize, "__results__")
+if (AlteryxFullUpdate) {
+  meta.data <- read.AlteryxMetaInfo("sim")
+  # Write out the metadata for the "pass-through" data
+  meta.data$Name <- as.character(meta.data$Name)
+  meta.data$Type <- as.character(meta.data$Type)
+  meta.data$Source <- as.character(meta.data$Source)
+  for (i in 1:nrow(meta.data))
+    write.AlteryxAddFieldMetaInfo(nOutput = 1, name = meta.data$Name[i], fieldType = meta.data$Type[i], size = meta.data$Size[i], source = meta.data$Source[i])
+  # Write out the metadata for the score field
+  score.field <- "asdfasdfresultsasdfasdf"
+  write.AlteryxAddFieldMetaInfo(nOutput = 1, name = score.field, fieldType = "Double", size = 8, source = "R-DATA:")
+} else {
+  chunkSize <- config$numRecords
+  valCount <- unlist(as.numeric(read.Alteryx("valCount")))[1]
+  isGLM <- valCount == 0
+  mult <- config$per.iter
+  seed <- config$seed
+  
+  ## Inputs ----
+  
+  
+  model <- unserializeObject(as.character(read.Alteryx("model")$Object[1]))
+  totalCount <- as.integer(unlist(read.Alteryx("totalCountSim", mode="list"))[1])
+  validation <- if (!isGLM) read.Alteryx("validation") else NULL
+  
+  
+  scoreProcess(model, isGLM, mult, totalCount, validation, seed, chunkSize)
+} 
